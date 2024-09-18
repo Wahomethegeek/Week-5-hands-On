fa48643a8416e92892d150c191480fdf3cc2bcbb
Wahomethegeek <gatwirikelvin@outlook.com>
2024-09-18 12:04:30 +0300

    Teach myself how to ignore confidential files

--- api/server.js
+++ api/server.js
@@ -10,8 +10,30 @@ app.use(cors())
 dotenv.config();
 
 
-app.get('', (req, res) => {
-    res.send("Welcome to the afternoon session")
+// app.get('', (req, res) => {
+//     res.send("Welcome to the afternoon session, I am happy to be here.")
+// })
+
+// Connection to the database
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+
+})
+
+// Check if connection works
+db.connect((err) => {
+    if(err) return console.log("Error connecting to MySQL")
+
+    console.log("Connected to MySQL as id: ", db.threadId)
+
+    //create database
+    db.query('CREATE DATABASE IF DOES NOT EXISTS expense_tracker', (err, result) => {
+        if(err) return console.log(err)
+
+        console.log("database expense_tracker created/checked");
+    })
 })
 
 app.listen(3000, (err) => {
